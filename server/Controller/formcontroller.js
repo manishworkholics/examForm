c23e6dac88835692ba91fb4c1043bafe6938ef88
Sarvesh Parmar <sarveshparmar@Sarveshs-MacBook-Air.local>
2023-04-11 12:56:03 +0530

    update api

--- server/Controller/formcontroller.js
+++ server/Controller/formcontroller.js
@@ -4,9 +4,11 @@ exports.formData = async(req,res)=>{
     const {firstname,middlename,lastname,email,phone,address,DOB,fathername,mothername,
         gender,maritalstatus,disability,category,domicile,cast,captcha} = req.body 
     try {
-        
+        const data = new user({firstname,middlename,lastname,email,phone,address,DOB,fathername,mothername,
+            gender,maritalstatus,disability,category,domicile,cast,captcha})
+        await data.save()
+        res.status(201).json({message:"Saved"})    
     } catch (error) {
-        
+        res.status(400).json({error:"Server Issue"})
     }    
-
 }
